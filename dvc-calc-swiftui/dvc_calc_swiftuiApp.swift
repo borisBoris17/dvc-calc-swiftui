88d739ed74757687f67cfc48afbbc8c11be79abc
Tucker <borisblade17@gmail.com>
2024-05-16 22:08:45 -0500

    First commit

---
+++ dvc-calc-swiftui/dvc_calc_swiftuiApp.swift
@@ -0,0 +1,17 @@
+//
+//  dvc_calc_swiftuiApp.swift
+//  dvc-calc-swiftui
+//
+//  Created by tucker bichsel on 16/05/2024.
+//
+
+import SwiftUI
+
+@main
+struct dvc_calc_swiftuiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
