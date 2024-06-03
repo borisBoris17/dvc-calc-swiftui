09285ba13bd685b6f7e89856396bc332048e8c83
Tucker <borisblade17@gmail.com>
2024-06-02 23:43:41 -0500

    Add SwiftData Models and pull in data from db generated from data creation app - Tucker

--- dvc-calc-swiftui/ContentView.swift
+++ dvc-calc-swiftui/ContentView.swift
@@ -6,16 +6,85 @@
 //
 
 import SwiftUI
+import SwiftData
 
 struct ContentView: View {
+    @Query var resorts: [Resort]
+    @Query var pointValues: [PointValue]
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
+        ScrollView {
+            Image("dvcCalc")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("Hello, world!")
+            
+            ForEach(resorts) { resort in
+                VStack {
+                    Text("\(resort.resortName) - \(resort.id)")
+                    
+                    ForEach(resort.roomTypes) { roomType in
+                        Text("\(roomType.roomName) - \(roomType.id)")
+                    }
+                }
+                Spacer()
+            }
+        
+            ForEach(pointValues) { pointValue in
+                VStack {
+                    HStack {
+                        Text(pointValue.startDate, format: .dateTime.month().day())
+                        Text(" - ")
+                        Text(pointValue.endDate, format: .dateTime.month().day())
+                    }
+                    
+                    Text("Weekday Rate: \(pointValue.weekdayRate)")
+                    Text("Weekend Rate: \(pointValue.weekendRate)")
+                }
+            }
         }
         .padding()
+    }
+}
+
+@Model
+class Resort {
+    var id: UUID
+    var resortName: String
+    var roomTypes: [RoomType]
+
+    init(id: UUID, name: String, roomTypes: [RoomType]) {
+        self.id = id
+        self.resortName = name
+        self.roomTypes = roomTypes
+    }
+}
+
+@Model
+class RoomType {
+    var id: UUID
+    var roomName: String
+    
+    init(id: UUID, name: String) {
+        self.id = id
+        self.roomName = name
+    }
+}
+
+@Model
+class PointValue {
+    // TODO: Add back when the new data is ready
+//    var id: UUID
+    var startDate: Date
+    var endDate: Date
+    var weekdayRate: Int16
+    var weekendRate: Int16
+    
+    init(id: UUID, startDate: Date, endDate: Date, weekdayRate: Int16, weekendRate: Int16) {
+//        self.id = id
+        self.startDate = startDate
+        self.endDate = endDate
+        self.weekdayRate = weekdayRate
+        self.weekendRate = weekendRate
     }
 }
 
--- dvc-calc-swiftui/dvc_calc_swiftuiApp.swift
+++ dvc-calc-swiftui/dvc_calc_swiftuiApp.swift
@@ -6,12 +6,29 @@
 //
 
 import SwiftUI
+import SwiftData
 
 @main
 struct dvc_calc_swiftuiApp: App {
+    let container: ModelContainer
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
+        }
+        .modelContainer(container)
+    }
+    
+    init() {
+        do {
+            guard let storeURL = Bundle.main.url(forResource: "dvc", withExtension: "store") else {
+                fatalError("Failed to find dvc.store")
+            }
+            
+            let config = ModelConfiguration(url: storeURL)
+            container = try ModelContainer(for: Resort.self, PointValue.self, configurations: config)
+        } catch {
+            fatalError("Failed to create model container: \(error)")
         }
     }
 }
